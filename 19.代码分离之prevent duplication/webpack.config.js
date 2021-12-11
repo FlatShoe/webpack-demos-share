47a4006f970d16b5ba6facbc3225788e7038b47b
FlatShoes <flatshoes@163.com>
2021-12-11 17:38:14 +0800

    代码分离之prevent duplication

--- 19.代码分离之prevent duplication/webpack.config.js
+++ 19.代码分离之prevent duplication/webpack.config.js
@@ -3,12 +3,19 @@ const {resolve} = require('path')
 module.exports = {
   mode: 'production',
   entry: {
-    index: {import: './src/index.js', dependOn: 'shared'},
-    main: {import: './src/main.js', dependOn: 'shared'},
-    shared: ['lodash']
+    // index: {import: './src/index.js', dependOn: 'shared'},
+    // main: {import: './src/main.js', dependOn: 'shared'},
+    // shared: ['lodash']
+    index: './src/index.js',
+    main: './src/main.js'
   },
   output: {
     filename: '[name].bundle.js',
     path: resolve(__dirname, 'build')
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
   }
 }
